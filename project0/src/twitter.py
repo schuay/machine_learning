689ef1d099b1fcb1a952f161e8982d06099805ba
Jakob Gruber <jakob.gruber@gmail.com>
2014-11-21 13:45:11 +0100

    Add twitter spam filtering

--- project0/src/twitter.py
+++ project0/src/twitter.py
@@ -1,4 +1,5 @@
 import classification_dataset as cd
+import re
 import tweet
 import zipfile
 
@@ -22,8 +23,11 @@ class TwitterInstance(cd.ClassificationInstanceI):
     def instance_class(self):
         return self.__instance_class
 
-def features(self):
+    def features(self):
         return self.__features
+
+    def text(self):
+        return self.__text
 
     def __str__(self):
         return "%d: %s" % (self.instance_class(), self.features())
@@ -32,15 +36,22 @@ class TwitterDataset(cd.ClassificationDatasetI):
     NEG = 0
     POS = 1
 
-    def __init__(self, data_archive, feature_selection, transformer):
+    def __init__(self, data_archive, max_instances,
+                 feature_selection, transformer):
         self.__instances = []
 
+        instance_count = 0
         with zipfile.ZipFile(data_archive, 'r') as f:
             datafile = f.open(COMPRESSED_FILENAME, "rU")
             next(datafile)  # Skip the header.
             for row in datafile:
                 self.__instances.append(
                     TwitterInstance(row, feature_selection, transformer))
+                instance_count += 1
+                if instance_count > max_instances:
+                    break
+
+        self.__filter_spam()
 
     def classes(self):
         return [POS, NEG]
@@ -50,6 +61,20 @@ class TwitterDataset(cd.ClassificationDatasetI):
 
     def name(self):
         return "twitter"
+
+    def __filter_spam(self):
+        PATTERN_SPAM1 = re.compile("Get 100 followers a day")
+        PATTERN_SPAM2 = re.compile("I highly recommends you join www.m2e.asia")
+        PATTERN_SPAM3 = re.compile("Banksyart2.*posting there since having probs")
+
+        FILTERS = [ lambda t: PATTERN_SPAM1.search(t)
+                  , lambda t: PATTERN_SPAM2.search(t)
+                  , lambda t: PATTERN_SPAM3.search(t)
+                  ]
+
+        return filter(
+                lambda t: not any([f(t.text()) for f in FILTERS]),
+                self.__instances)
 
 import featureselection as fs
 import sys
@@ -62,4 +87,6 @@ if __name__ == '__main__':
                                 ])
     transformer = tr.IdentityTransformer()
 
-    dataset = TwitterDataset(sys.argv[1], feature_selection, transformer)
+    dataset = TwitterDataset(sys.argv[1], 1000, feature_selection, transformer)
+    for instance in dataset.instances():
+        print(instance)
