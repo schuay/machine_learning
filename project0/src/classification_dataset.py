b7e9caa59e7b4ab2dee60b631a16dc246833a63b
Jakob Gruber <jakob.gruber@gmail.com>
2014-11-21 12:13:11 +0100

    Add interfaces for classification datasets and instances

---
+++ project0/src/classification_dataset.py
@@ -0,0 +1,21 @@
+class ClassificationInstanceI:
+    def class(self):
+        """Returns the class of this instance."""
+        raise NotImplementedError("Please implement this yourself.")
+
+    def features(self):
+        """Returns a dictionary mapping features to values."""
+        raise NotImplementedError("Please implement this yourself.")
+
+class ClassificationDatasetI:
+    def classes(self):
+        """Returns a list of all classes in this dataset."""
+        raise NotImplementedError("Please implement this yourself.")
+
+    def instances(self):
+        """Returns a list of all instances in this dataset."""
+        raise NotImplementedError("Please implement this yourself.")
+
+    def name(self):
+        """Returns the name of this dataset."""
+        raise NotImplementedError("Please implement this yourself.")
