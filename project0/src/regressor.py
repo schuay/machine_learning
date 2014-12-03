50613b3e1d664c5d7fabd097501c33ee83764cb6
b3311628 <b3311628@trbvm.com>
2014-12-03 22:20:33 +0100

    Update regressor to work similarly to classifier

--- project0/src/regressor.py
+++ project0/src/regressor.py
@@ -4,14 +4,16 @@ import getopt
 import sys
 import time
 
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 
 import dataset_splitter as ds
 import power_consumption as pc
 import solar_flares as sf
 
-DSETS_DEFAULT  = 'solar_flares'
-CUTOFF_DEFAULT = 0.75
+REGRE_DEFAULT  = 'linear'
+DSETS_DEFAULT  = 'power_consumption'
+SPLIT_DEFAULT  = 'ratio75'
+LIMIT_DEFAULT  = 50000
 
 DATASETS = { 'power_consumption': lambda mi: pc.PowerConsumptionDataset(
                     '../data/power_consumption/household_power_consumption.zip',
@@ -19,6 +21,23 @@ DATASETS = { 'power_consumption': lambda mi: pc.PowerConsumptionDataset(
            , 'solar_flares': lambda mi: sf.SolarFlaresDataset(
                     '../data/solar_flares/flare.data2')
            }
+
+REGRESSORS = { 'linear': LinearRegression()
+             }
+
+SPLITTERS = { 'ratio75': ds.RatioSplitter(75)
+            , 'ratiorange': ds.RatioRangeSplitter(5, 96, 5)
+            , '10fold':  ds.CrossfoldSplitter(10)
+            }
+
+class Opts:
+    dataset = DSETS_DEFAULT
+    limit = LIMIT_DEFAULT
+    regressor = REGRE_DEFAULT
+    splitter = SPLIT_DEFAULT
+    verbose = False
+
+options = Opts()
 
 class Regressor:
     def __init__(self, clf, train_size):
@@ -35,7 +54,7 @@ class Regressor:
     def evaluate(self, test_sets):
         squares = list()
         for inst in test_sets.instances():
-            predictions = [ int(val) for val in self.__clf.predict(inst.x()) ]
+            predictions = [ val for val in self.__clf.predict(inst.x()) ]
             print("ACTUAL: %s PREDICTED: %s FEATURES: %s" %
                     (inst.y(), predictions, inst.x()))
             squares.append([(y - py)**2 for y,py in zip(inst.y(), predictions)])
@@ -55,27 +74,39 @@ def evaluate_features(dataset, splitter, raw_regressor):
         regressor.evaluate(test_set)
 
 def usage():
-    print("""USAGE: %s
-            TODO""" % sys.argv[0])
+    print("""USAGE: %s [options]
+            -d  The dataset to use. One of 'power_consumption' (default), 'solar_flares'.
+            -s  Selects the splitter. One of 'ratio75' (default), '10fold', 'ratiorange'.
+            -t  Selects the regressor type. One of 'linear' (default).
+            -l  Limit the number of rows loaded.
+            -v  Verbose output.""" %
+            ( sys.argv[0]
+            ))
     sys.exit(1)
 
 if __name__ == '__main__':
-    dataset_ctor = DATASETS[DSETS_DEFAULT]
-    cutoff = CUTOFF_DEFAULT
-
-    opts, args = getopt.getopt(sys.argv[1:], "c:d:h")
+    opts, args = getopt.getopt(sys.argv[1:], "d:l:t:s:vh")
     for o, a in opts:
         if o == "-d":
             if not a in DATASETS:
                 usage()
-            dataset_ctor = DATASETS[a]
-        elif o == "-c":
-            cutoff = float(a)
+            options.dataset = a
+        elif o == "-l":
+            options.limit = int(a)
+        elif o == "-t":
+            if not a in REGRESSORS:
+                usage()
+            options.regressor = a
+        elif o == "-s":
+            if not a in SPLITTERS:
+                usage()
+            options.splitter = a
+        elif o == "-v":
+            options.verbose = True
         else:
             usage()
 
-    splitter = ds.RatioSplitter(0.75)
-    evaluate_features( dataset_ctor(10000)
-                     , splitter
-                     , linear_model.LinearRegression()
+    evaluate_features( DATASETS[options.dataset](options.limit)
+                     , SPLITTERS[options.splitter]
+                     , REGRESSORS[options.regressor]
                      )
