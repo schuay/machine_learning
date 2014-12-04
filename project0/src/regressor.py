f58a8bf7a4398f08e650957ac0e5052fd6793d45
b3311628 <b3311628@trbvm.com>
2014-12-04 03:24:50 +0100

    Add metrics and new model to regressor

--- project0/src/regressor.py
+++ project0/src/regressor.py
@@ -5,6 +5,11 @@ import sys
 import time
 
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import RadiusNeighborsRegressor
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
 
 import dataset_splitter as ds
 import power_consumption as pc
@@ -13,6 +18,7 @@ import solar_flares as sf
 REGRE_DEFAULT  = 'linear'
 DSETS_DEFAULT  = 'power_consumption'
 SPLIT_DEFAULT  = 'ratio75'
+MVALS_DEFAULT  = 'mean'
 LIMIT_DEFAULT  = 50000
 
 DATASETS = { 'power_consumption': lambda mi: pc.PowerConsumptionDataset(
@@ -23,6 +29,7 @@ DATASETS = { 'power_consumption': lambda mi: pc.PowerConsumptionDataset(
            }
 
 REGRESSORS = { 'linear': LinearRegression()
+             , 'knnradius': RadiusNeighborsRegressor(radius=1)
              }
 
 SPLITTERS = { 'ratio75': ds.RatioSplitter(75)
@@ -30,49 +37,62 @@ SPLITTERS = { 'ratio75': ds.RatioSplitter(75)
             , '10fold':  ds.CrossfoldSplitter(10)
             }
 
+MISSING_VALUES_STRATEGY = { 'mean': Imputer(strategy='mean')
+                          , 'median': Imputer(strategy='median')
+                          }
+
 class Opts:
     dataset = DSETS_DEFAULT
     limit = LIMIT_DEFAULT
     regressor = REGRE_DEFAULT
     splitter = SPLIT_DEFAULT
+    mvals = MVALS_DEFAULT
     verbose = False
 
 options = Opts()
 
 class Regressor:
-    def __init__(self, clf, train_size):
+    def __init__(self, clf, mvals, train_size):
         self.__clf = clf
+        self.__mvals = mvals
         self.__train_size = train_size
 
     @staticmethod
-    def train(clf, training_sets):
+    def train(clf, mvals, training_sets):
         xs = [ inst.x() for inst in training_sets.instances() ]
         ys = [ inst.y() for inst in training_sets.instances() ]
-        return Regressor(clf.fit(xs, ys), len(xs))
+
+        mvals = mvals.fit(ys)
+        ys = mvals.transform(ys)
+
+        return Regressor(clf.fit(xs, ys), mvals, len(xs))
 
     """Evaluates the classifier with the given data sets."""
     def evaluate(self, test_sets):
-        squares = list()
-        for inst in test_sets.instances():
-            predictions = [ val for val in self.__clf.predict(inst.x()) ]
-            print("ACTUAL: %s PREDICTED: %s FEATURES: %s" %
-                    (inst.y(), predictions, inst.x()))
-            squares.append([(y - py)**2 for y,py in zip(inst.y(), predictions)])
-        print map(lambda l: sum(l)/len(l), zip(*squares))
-
         xs = [ inst.x() for inst in test_sets.instances() ]
         ys = [ inst.y() for inst in test_sets.instances() ]
-        print self.__clf.score(xs,ys)
+        ys = self.__mvals.transform(ys)
+        ps = self.__clf.predict(xs)
+
+        if options.verbose:
+            for x, y, p in zip(xs, ys, ps):
+                print("ACTUAL: %s PREDICTED: %s FEATURES: %s" % (y, p, x))
+
+        yp = zip(zip(*ys), zip(*ps))
+        print map(lambda (y, p): metrics.mean_absolute_error(y,p), yp)
+        print map(lambda (y, p): metrics.mean_squared_error(y,p), yp)
+        print map(lambda (y, p): metrics.r2_score(y,p), yp)
 
     def classify(self, obj):
         return self.__nltk_classifier.classify(obj)
 
-def evaluate_features(dataset, splitter, raw_regressor):
+def evaluate_features(dataset, splitter, raw_regressor, mvals):
     dataset_tuples = splitter.split(dataset)
+    raw_regressor = Pipeline([('norm',Normalizer()), ('regr', raw_regressor)])
 
     for (train_set, test_set) in dataset_tuples:
         print 'training new regressor'
-        regressor = Regressor.train(raw_regressor, train_set);
+        regressor = Regressor.train(raw_regressor, mvals, train_set);
 
         print 'testing regressor...'
         regressor.evaluate(test_set)
@@ -89,14 +109,14 @@ def usage():
     sys.exit(1)
 
 if __name__ == '__main__':
-    opts, args = getopt.getopt(sys.argv[1:], "d:l:t:s:vh")
+    opts, args = getopt.getopt(sys.argv[1:], "d:l:t:s:m:vh")
     for o, a in opts:
         if o == "-d":
             if not a in DATASETS:
                 usage()
             options.dataset = a
         elif o == "-l":
-            options.limit = int(a)
+            options.limit = None if int(a) == 0 else int(a)
         elif o == "-t":
             if not a in REGRESSORS:
                 usage()
@@ -105,6 +125,10 @@ if __name__ == '__main__':
             if not a in SPLITTERS:
                 usage()
             options.splitter = a
+        elif o == "-m":
+            if not a in MISSING_VALUES_STRATEGY:
+                usage()
+            options.mvals = a
         elif o == "-v":
             options.verbose = True
         else:
@@ -113,4 +137,5 @@ if __name__ == '__main__':
     evaluate_features( DATASETS[options.dataset](options.limit)
                      , SPLITTERS[options.splitter]
                      , REGRESSORS[options.regressor]
+                     , MISSING_VALUES_STRATEGY[options.mvals]
                      )
